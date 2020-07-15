3751ae6663b47ef9cde04c5907a435eacbd5cc1d
Mahendra Mahajan <piushmahajan@gmail.com>
2020-07-15 15:10:48 +0000

    Created my first python program, firstprogram.py

---
+++ firstprogram.py
@@ -0,0 +1,24 @@
+# .py is the extension for any python file,
+# print("Mahendra Mahajan")
+# this is a program that print my name.
+# when we press run button above, repl.it takes this
+# file firstprogram.py and pass the source code to the python interpreter that repl.it already has.
+# The interpreter then runs it, genrate the Byte code and passes that to CPyhton VM which is showing the results to the console.
+
+#  python firstprogram.py
+# Mahendra Mahajan
+# --> This is how we run a python program, main.py is the file name.
+#  python --version
+# Python 3.8.3
+# --> To know which version of python interpreter you are using, use the --version option with python command.
+
+name = input("What is your name?")
+print("Your name is ", name)
+
+#  python firstprogram.py
+# What is your name? Mahendra Mahajan
+# Your name is  Mahendra Mahajan
+
+# input() --> read the string from the standard input i.e. keyboard
+# then name = input(), here name is a variable used to store the input recieved from the keyboard.
+# then print(name), printing the value stored in the variable to the standard console.
